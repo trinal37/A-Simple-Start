9d0bd40ef7bf47a11954d99ca80c58c623f081e7
Janelle <jblea37@yahoo.com>
2011-09-19 22:39:54 -0600

    testing

---
+++ ex3.rb
@@ -0,0 +1,37 @@
+# display 1 line of text
+puts "I will now count my chickens:"
+
+# display 'hens' on one line, the comma seperates the math to the next line
+puts "Hens", 25 + 30 / 6
+# again text then math on next line
+puts "Roosters", 100 - 25 * 3 % 4
+
+# display text
+puts "Now I will count the eggs:"
+
+# more maaaaaaath, though '%' makes my head hurt
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# display text
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# display equation answer
+puts 3 + 2 < 5 - 7
+
+# display text and answer on next line
+puts "What is 3 + 2?", 3 + 2
+# display text and answer on next line again
+puts "What is 5 - 7?", 5 - 7
+
+# more text
+puts "Oh, that's why it's false."
+
+# still more text
+puts "How about some more."
+
+# text then 'true/false' answer on next line
+puts "Is it greater?", 5 > -2
+# more text and another 't/f' question
+puts "Is it greater or equal?", 5 >= -2
+# last question, 't/f'
+puts "Is it less or equal?", 5 <= -2
